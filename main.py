42cb33c979eb2c78ea97157300105d24e0289d7a
greceanu.anamaria1 <greceanu.anamaria1@gmail.com>
2023-07-12 15:06:31 +0300

    display the word in case of losing + changing the name of variables

--- main.py
+++ main.py
@@ -1,67 +1,65 @@
 from random_word import RandomWords
 import time
 
-joc=True
-print("Introduceti numele: ")
-print("Bun venit",input())
+print("Enter your name ")
+print("Welcome",input())
 
 
-def generare():
-    print("Ti se genereaza cuvantul...")
+def wordLoading():
+    print("Your word is...")
     time.sleep(2)
-    cuvant=RandomWords().get_random_word()
-    # print(cuvant)
-    return cuvant
+    word=RandomWords().get_random_word()
+    print(word)
+    return word
 
-def afisare():
-    cuvant_gasit=""
-    for c in cuvant:
-        if c in litere_folosite or c==cuvant[0] or c==cuvant[len(cuvant)-1]:
-            cuvant_gasit+=(c)
+def display():
+    guessed_word=""
+    for letter in word:
+        if letter in used_letters or letter==word[0] or letter==word[len(word)-1]:
+            guessed_word+=(letter)
         else:
-            cuvant_gasit+=("_")
-    print(cuvant_gasit)
-    print("vieti:",str(vieti))
-    print("ai folosit urmatoarele litere ", litere_folosite)
-    return cuvant_gasit
+            guessed_word+=("_")
+    print(guessed_word)
+    print("Lives:",str(lives))
+    print("You have used the following letters ", used_letters)
+    return guessed_word
 
-cuvinte_gasite=[]
+words_found=[]
 
 while True:
-    cuvant=generare()
-    vieti=5
-    litere_folosite=[] 
-    afisare()
+    word=wordLoading()
+    lives=5
+    used_letters=[] 
+    display()
 
-    while vieti>0:
-        ghici=input("\nIntrodu o litera sau cuvantul: ")
-        if ghici==cuvant:
-            print("\n Felicitari, ai ghicit cuvantul!")
+    while lives>0:
+        guess=input("\nEnter a letter or the entire word: ").lower()
+        if guess==word:
+            print("\nCongratulations, you guessed the word!")
             break
-        elif len(ghici)>1:
-            vieti-=1
-            afisare()
-        elif ghici in cuvant and ghici in [cuvant[0],cuvant[len(cuvant)-1]]:
-            print("Litera ti a fost data la inceput!")
-        elif ghici in cuvant:
-            litere_folosite.append(ghici)
-            cuvant_gasit=afisare()
-            if "_" not in cuvant_gasit:
-                print("\n Felicitari, ai ghicit cuvantul!")
+        elif len(guess)>1:
+            lives-=1
+            display()
+        elif guess in word and guess in [word[0],word[len(word)-1]]:
+            print("The letter was given by the game!")
+        elif guess in word:
+            used_letters.append(guess)
+            guessed_word=display()
+            if "_" not in guessed_word:
+                print("\nCongratulations, you guessed the word!")
                 break
         else:
-            vieti-=1
-            litere_folosite.append(ghici)
-            afisare()
-    if vieti==0:
-        print("ai pierdut")
+            lives-=1
+            used_letters.append(guess)
+            display()
+    if lives==0:
+        print("You lost:( The word was '"+ word+"'")
     else:
-        cuvinte_gasite.append(cuvant)
+        words_found.append(word)
 
-    print("\ncuvinte ghicite pana acum: ",cuvinte_gasite)
-    raspuns=input("vrei sa continui?(da/nu): ")
-    if raspuns=="nu":
+    print("\nGuessed words in this session:",words_found)
+    answer=input("Do you still want to play?('yes'/any other key for 'no'): ").lower()
+    if answer=="yes":
+        continue
+    else:
         break
-
-
-
